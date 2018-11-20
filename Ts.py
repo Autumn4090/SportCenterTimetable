1af0574198f86c1ed8ac0b4afd3ae779546d4c57
LarryG <lerryun@gmail.com>
2018-11-19 19:38:36 -0800

    Minor changes

--- Ts.py
+++ Ts.py
@@ -8,15 +8,12 @@ from SportCenter import SportCenter
 
 s = requests.Session()
 
-
 class Main(QMainWindow, MainWindow.Ui_MainWindow):
 	def __init__(self):
 		super(self.__class__, self).__init__()
 		self.setupUi(self)
 		self.btn.clicked.connect(self.load)
-		# self.tableWidget.doubleClicked.connect(self.on_click)
-		# self.tableWidget.itemChanged.connect(self.on_click)
-		# self.tableWidget.cellClicked.connect(self.on_click)
+		self.tableWidget.cellDoubleClicked.connect(self.on_click)
 
 	def load(self):
 		floor = self.cbox.currentText()
@@ -24,24 +21,22 @@ class Main(QMainWindow, MainWindow.Ui_MainWindow):
 		data = sc.parse_timetable(sc.get_timetable(floor))
 		self.connect_table_items(self.tableWidget, data)
 
-
-	def connect_table_items(self, widget, data):
+	def update_table_items(self, widget, data):
 		i = 0
 		for row in range(0, 15):
 			for col in range(0, 8):
 				self.tableWidget.item(row, col).setText(data[i])
-				# This one is need for updating text on mac
-				# I dont know why
-				app.processEvents()
 				i += 1
+			app.processEvents()
+			# This one is needed for updating text on mac
+			# I dont know why
 
 	def on_click(self, row ,column):
-		# Not working
-		print(self.tableWidget.itemAt(row, column).text())
+		print(self, row, column)
+		print(self.tableWidget.item(row, column).text())
 
 	def main(self):
 		pass
-
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
